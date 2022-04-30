312cf8700ca3567856d79fd93d029d43d28853ff
NBValero <97256245+NBValero@users.noreply.github.com>
2022-04-30 17:34:09 +0200

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,72 @@
+[
+  {
+    "Clases": "Pan y cereales",
+    "Total": "99,271",
+    "Variacion": 1
+  },
+  {
+    "Clases": "Carne",
+    "Total": "98,668",
+    "Variacion": 3.5
+  },
+  {
+    "Clases": "Pescado y marisco",
+    "Total": "98,361",
+    "Variacion": 2.3
+  },
+  {
+    "Clases": "Leche, queso y huevos",
+    "Total": "99,378",
+    "Variacion": 1.1
+  },
+  {
+    "Clases": "Aceites y grasas",
+    "Total": "87,951",
+    "Variacion": -5.5
+  },
+  {
+    "Clases": "Frutas",
+    "Total": "96,865",
+    "Variacion": 7.6
+  },
+  {
+    "Clases": "Legumbres y hortalizas",
+    "Total": "99,015",
+    "Variacion": 1.6
+  },
+  {
+    "Clases": "Azúcar, confitura, miel, chocolate y confitería",
+    "Total": "99,586",
+    "Variacion": 1.1
+  },
+  {
+    "Clases": "Productos alimenticios n.c.o.p.",
+    "Total": "99,659",
+    "Variacion": 1.2
+  },
+  {
+    "Clases": "Café, té y cacao",
+    "Total": "99,538",
+    "Variacion": 0.8
+  },
+  {
+    "Clases": "Agua mineral, refrescos, zumos de frutas y vegetales",
+    "Total": "93,519",
+    "Variacion": 2.1
+  },
+  {
+    "Clases": "Bebidas destiladas",
+    "Total": "101,327",
+    "Variacion": 1.1
+  },
+  {
+    "Clases": "Vino",
+    "Total": "102,134",
+    "Variacion": -0.2
+  },
+  {
+    "Clases": "Cerveza",
+    "Total": "99,125",
+    "Variacion": 3.2
+  }
+]
